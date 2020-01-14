328d7e59e300c61b8b0c361373b845b86aab6045
이은지 <eunji.lee@ieunjiui-MacBook-Pro.local>
2020-01-14 21:23:50 +0900

    eslint&prettier setting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-     Hello world!
-     Dev Branch
-    </div>
-  );
+  return <div className="App">Hello world! Dev Branch</div>;
 }
-
+console.log('');
 export default App;
