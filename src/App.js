b75268f38f74ebcc3652a7d140c1d85e13e3385d
이은지 <eunji.lee@ieunjiui-MacBook-Pro.local>
2020-01-15 14:39:44 +0900

    Add SignUpInfo use antd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import LoginInfo from './component/Main/LoginInfo';
+import SignUpInfo from './component/Signup/SignUpInfo';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <LoginInfo>Login</LoginInfo>
+        <SignUpInfo>SignUp</SignUpInfo>
       </div>
     );
   }
