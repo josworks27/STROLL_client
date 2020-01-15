5c6a32ca8ddda0fb0f2689ba809727a688ff1b9f
JY-Ahn <junyongsw92@gmail.com>
2020-01-15 14:39:41 +0900

    App.js Routing - only for LoginInfo Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import LoginInfo from './component/Main/LoginInfo';
+import { Route, Switch, Link } from 'react-router-dom';
+// import { LoginInfo } from './index';
+// import Routes from './Route/Routes';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <LoginInfo>Login</LoginInfo>
+        <Switch>
+          <Route path='/' component={LoginInfo} />
+        </Switch>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './Root';
 
-// import './LoginInfo.css';
-
-import App from './App';
 
 // eslint-disable-next-line no-undef
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
-
-// ReactDOM.render(<WrappedNormalLoginForm />, document.getElementById('root'));
+export { default as LoginInfo } from './component/Main/LoginInfo';
+// export { default as LoginInfo } from './component/AddTrail/Info_Map';
+// export { default as LoginInfo } from './component/AddTrail//Info_Trail_Input';
+// export { default as LoginInfo } from './component/Mypage/';
+// export { default as LoginInfo } from './component/Main/LoginInfo';
+// export { default as LoginInfo } from './component/Main/LoginInfo';
