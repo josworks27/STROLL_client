2343774580f5e23207a3267c162dd431e9f8d743
JY-Ahn <junyongsw92@gmail.com>
2020-01-16 16:48:51 +0900

    kakao map visualization on whole pate

--- src/component/Mypage/TrailListElement.js
+++ src/component/Mypage/TrailListElement.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'antd';
+import { Link } from 'react-router-dom';
 
 // const { Header, Content, Footer, Sider } = Layout;
 export default class TrailListElement extends Component {
   render() {
     return (
-      <Row id='id_Mypage_traillistElement'>
-        <Col span={18} className="cl_trail">{this.props.trail}</Col>
+      <Row id="id_Mypage_traillistElement">
+        <Link to='/trailinfo'>
+          <Col span={18} className="cl_trail" onClick={() => {}}></Col>
+        </Link>
       </Row>
     );
   }
