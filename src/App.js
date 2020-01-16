85b0feae5830ac8ff29b86d6018d71dde4e16e7f
JY-Ahn <junyongsw92@gmail.com>
2020-01-16 10:38:27 +0900

    Trailinfo page - try to use naver map api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import login_page from './pages/login';
 import signup_page from './pages/signup';
+import trailinfo_page from './pages/trailinfo'
 // import {addTrail_page, login_page, mypage_page, signup_page, trailinfo_page} from './pages'
 // import LoginInfo from './component/Main/LoginInfo';
 // import SignUpInfo from './component/Signup/SignUpInfo';
 import { Route, Switch, Link } from 'react-router-dom';
 // import { LoginInfo } from './index';
 // import Routes from './Route/Routes';
+import './index.css';
+
+
+
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div id='appjs'>
         <Route exact path='/' component={login_page} />
         <Switch>
           <Route path='/signup' component={signup_page} />
+          <Route path='/trailinfo' component={trailinfo_page}/>
           {/* <Route path='/signup' component={SignUpInfo} />
           <Route path='/signup' component={SignUpInfo} />
           <Route path='/signup' component={SignUpInfo} /> */}
