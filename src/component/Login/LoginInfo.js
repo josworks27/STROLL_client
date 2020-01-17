0d8e6af39a2a3967a89dbf87e071ee19bd8ac236
이은지 <eunji.lee@ieunjiui-MacBook-Pro.local>
2020-01-17 18:36:21 +0900

    ✨Add Login function use fakedata and axios

--- src/component/Login/LoginInfo.js
+++ src/component/Login/LoginInfo.js
@@ -1,38 +1,65 @@
 import React, { Component } from 'react';
-import { Form, Icon, Input, Button, Layout } from 'antd';
+import { Link } from 'react-router-dom';
+
+import { Form, Icon, Input, Button, Alert } from 'antd';
 import 'antd/dist/antd.css';
 import './LoginInfo.css';
-import { Link} from 'react-router-dom';
-// import { Redirect } from 'react-router-dom';
-const { Header, Footer, Content } = Layout;
+
+import axios from 'axios';
 
 class LoginInfo extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { email: null, password: null };
   }
-
-  handleSubmit(e) {
+  // 서브밋 메소드
+  handleSubmit = e => {
     e.preventDefault();
-  }
-
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+        var loginData = JSON.stringify(values);
+        axios
+          .post('/signin', loginData, {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          })
+          .then(res => {
+            if (res.status === 200) {
+              // 로그인 스테이트 true로 변경
+              this.props.handleIsloginState();
+              // TODO: isLogin state가 true이면 /main으로 가도록 리디렉션
+            }
+          });
+      }
+    });
+  };
+  // 인풋값 매소드
+  handleInputValue = key => e => {
+    this.setState({ [key]: e.target.value });
+  };
+  // 얼러트 매소드
+  onClose = e => {
+    console.log(e, 'I was closed.');
+  };
   render() {
+    console.log(this.props.isLogin);
     const { getFieldDecorator } = this.props.form;
     return (
-      <div id='id_wrapper'>
+      <div id="id_wrapper">
         <div className="cl_STROLL">STROLL 🍃</div>
         <Form onSubmit={this.handleSubmit} className="login-form">
           <Form.Item>
-            {getFieldDecorator('username', {
-              rules: [
-                { required: true, message: 'Please input your username!' },
-              ],
+            {getFieldDecorator('email', {
+              rules: [{ required: true, message: 'Please input your email!' }],
             })(
               <Input
                 prefix={
                   <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
                 }
-                placeholder="Username"
+                placeholder="email"
+                onChange={this.handleInputValue('email')}
               />,
             )}
           </Form.Item>
@@ -48,21 +75,42 @@ class LoginInfo extends Component {
                 }
                 type="password"
                 placeholder="Password"
+                onChange={this.handleInputValue('password')}
               />,
             )}
           </Form.Item>
           <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
-            >
-              <Link to="/main">Log in</Link>
-            </Button>
+            {this.props.isLogin ? (
+              <Link to="/main">
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                >
+                  Log in
+                </Button>
+              </Link>
+            ) : (
+              <Link to="/">
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                >
+                  Log in
+                  {/* TODO: 현재는 프롭스의 상태에 따라 경고창을 띄워 그냥 접속시에도 보이지만  
+                            나중에는 스테이터스 코드에 따른 분기를 처리해 상황에 따라 경고창을 띄우도록 한다.*/}
+                  <Alert
+                    message="Invaild user !"
+                    type="error"
+                    closable
+                    onClose={this.onClose}
+                  />
+                </Button>
+              </Link>
+            )}
             Or
-            <Link to="/signup">
-              <a href=""> register now!</a>
-            </Link>
+            <Link to="/signup"> register now!</Link>
           </Form.Item>
         </Form>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-/* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
 import './index.css';
 
-// eslint-disable-next-line no-undef
 ReactDOM.render(<Root />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
-export { default as LoginInfo } from './component/Main/LoginInfo';
-// export { default as LoginInfo } from './component/AddTrail/Info_Map';
-// export { default as LoginInfo } from './component/AddTrail//Info_Trail_Input';
-// export { default as LoginInfo } from './component/Mypage/';
-// export { default as LoginInfo } from './component/Main/LoginInfo';
-// export { default as LoginInfo } from './component/Main/LoginInfo';
+export { default as LoginInfo } from './component/Login/LoginInfo';
